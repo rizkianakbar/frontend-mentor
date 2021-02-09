fa2210adb64f97395fc9bc43cd4024f096ed6263
rizkianakbar <rizkianakbar4@gmail.com>
2021-02-09 16:24:03 +0700

    add animation on hamburger button

--- easybank-landing-page/app/js/script.js
+++ easybank-landing-page/app/js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const btnHamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click', function() {
+    console.log('open hamburger');
+    if (btnHamburger.classList.contains('open')) {
+        btnHamburger.classList.remove('open');
+    } else {
+        btnHamburger.classList.add('open');
+    }
+});
